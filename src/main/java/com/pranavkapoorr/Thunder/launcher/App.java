22c44d8a0244b4dba1f573e4200a8d66baafb7d7
Pranav Kapoor <pranavkapoorr@gmail.com>
2018-09-15 21:25:07 -0400

    first commit

---
+++ src/main/java/com/pranavkapoorr/Thunder/launcher/App.java
@@ -0,0 +1,13 @@
+package com.pranavkapoorr.Thunder.launcher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
